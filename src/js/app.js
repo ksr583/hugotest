8956d30d23845a0f80f6d8c709ff8b344a8e03fa
Kyle Robinson <kylerobinson583@gmail.com>
2018-06-04 15:32:42 -0500

    set up initial structure

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,23 @@
 // JS Goes here - ES6 supported
 
-// Say hello
-console.log("🦊 Hello! Edit me in src/js/app.js");
+document
+  .getElementById('js-nav-toggle')
+  .addEventListener('click', (e) => {
+
+    let className = 'nav-open';
+    let el = document.getElementById('js-body');
+
+    if (el.classList) {
+      el.classList.toggle(className);
+    } else {
+      var classes = el.className.split(' ');
+      var existingIndex = classes.indexOf(className);
+
+      if (existingIndex >= 0)
+        classes.splice(existingIndex, 1);
+      else
+        classes.push(className);
+
+      el.className = classes.join(' ');
+    }
+  });
